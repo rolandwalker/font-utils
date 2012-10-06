89ef667af8bd4ea3b369f8bf1e53d6fd18ada570
Roland Walker <walker@pobox.com>
2012-10-05 20:22:23 -0400

    bump version

--- font-utils.el
+++ font-utils.el
@@ -5,7 +5,7 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/font-utils
 ;; URL: http://raw.github.com/rolandwalker/font-utils/master/font-utils.el
-;; Version: 0.6.4
+;; Version: 0.6.6
 ;; Last-Updated:  5 Oct 2012
 ;; EmacsWiki: FontUtils
 ;; Package-Requires: ((persistent-soft "0.8.6") (pcache "0.2.3"))
@@ -140,7 +140,7 @@
 ;;;###autoload
 (defgroup font-utils nil
   "Utility functions for working with fonts."
-  :version "0.6.4"
+  :version "0.6.6"
   :link '(emacs-commentary-link "font-utils")
   :prefix "font-utils-"
   :group 'extensions)
